a21110d9b091a5f8ccb8694daffcc7e57cb5b492
Joe Ferraro <ferraro.joseph@gmail.com>
2014-10-28 12:28:32 -0400

    pre-alpha index metadata

--- cmds/index-metadata.js
+++ cmds/index-metadata.js
@@ -5,29 +5,33 @@
 
 var util 							= require('../lib/util').instance;
 var Project 					= require('../lib/project');
-var Metadata 					= require('../lib/metadata');
 
-module.exports = function(program) {
+var Command = function(){};
 
+Command.execute = function(command) {
+	var self = command;
+
+	global.project = new Project();
+	global.project.initialize()
+		.then(function() {
+			return global.project.indexMetadata();
+		})
+		.then(function() {
+			util.respond(self, 'Metadata successfully indexed');
+		})
+		['catch'](function(error) {
+			util.respond(self, 'Could not index metadata', false, error);
+		})
+		.done();
+};
+
+exports.command = Command;
+exports.addSubCommand = function(program) {
 	program
 		.command('index-metadata')
 		.version('0.0.1')
 		.description('Indexes project\'s metadata')
 		.action(function(/* Args here */){
-			var self = this;
-
-			global.project = new Project();
-			global.project.initialize()
-				.then(function() {
-					return Metadata.index();
-				})
-				.then(function(res) {
-					util.respond(self, 'ok');
-				})
-				['catch'](function(error) {
-					util.respond(self, 'Could not index metadata', false, error);
-				})
-				.done();
+			Command.execute(this);	
 		});
-	
 };
