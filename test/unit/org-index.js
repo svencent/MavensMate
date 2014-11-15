424bdbddf84b8f681a933dbc0c236daaca9b6569
Joe Ferraro <ferraro.joseph@gmail.com>
2014-11-15 15:02:13 -0500

    update org index test

--- test/unit/org-index.js
+++ test/unit/org-index.js
@@ -25,7 +25,7 @@ describe('mavensmate org-index', function(){
 
 	});
 
-	it.only('should index', function(done) {
+	it('should index different types of metadata', function(done) {
 
 		this.timeout(100000);
 
@@ -33,9 +33,30 @@ describe('mavensmate org-index', function(){
 
 		helper.setProject(testClient, 'existing-project', function() {
 			var indexService = new IndexService({project:testClient.getProject()});
-			indexService.indexServerProperties(['SynonymDictionary'])
+			indexService.indexServerProperties(['ApexClass', 'CustomObject', 'Report'])
 				.then(function(res) {
-					console.log(res);
+					var apexClassResult = res[0];
+					var customObjectResult = res[1];
+					var reportResult = res[2];
+
+					apexClassResult.title.should.equal('ApexClass');
+					apexClassResult.isFolder.should.equal(true);
+					apexClassResult.cls.should.equal('folder');
+					apexClassResult.children.length.should.be.at.least(1);
+
+					customObjectResult.title.should.equal('CustomObject');
+					customObjectResult.isFolder.should.equal(true);
+					customObjectResult.inFolder.should.equal(false);
+					customObjectResult.hasChildTypes.should.equal(true);
+					customObjectResult.cls.should.equal('folder');
+					customObjectResult.children.length.should.be.at.least(1);
+
+					reportResult.title.should.equal('CustomObject');
+					reportResult.isFolder.should.equal(true);
+					reportResult.inFolder.should.equal(true);
+					reportResult.hasChildTypes.should.equal(false);
+					reportResult.cls.should.equal('folder');
+					reportResult.children.length.should.be.at.least(1);
 				})
 				['catch'](function(err) {
 					console.log(err);
