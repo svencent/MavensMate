3abd5df0e6319d2a4027593ece6151ec61ba5c8f
Joe Ferraro <ferraro.joseph@gmail.com>
2014-11-24 20:49:46 -0500

    adds meta-xml compilation via the metadata api

--- test/commands/compile-metadata.js
+++ test/commands/compile-metadata.js
@@ -10,7 +10,7 @@ describe('mavensmate compile-metadata', function(){
   var testClient = helper.createClient('atom');
   helper.ensureTestProject(testClient, 'compile-metadata');
 
-  it('should compile a list of files', function(done) {
+  it('should compile an apex class via the tooling api', function(done) {
 
     helper.unlinkEditor();
     this.timeout(20000);      
@@ -25,6 +25,33 @@ describe('mavensmate compile-metadata', function(){
             should.equal(err, null);
             response.should.have.property('result');
             response.result[0].State.should.equal('Completed');
+            done();
+          });
+        })
+        .done();
+    });
+    
+    var filesToDelete = [path.join(helper.baseTestDirectory(),'workspace', 'compile-metadata', 'src', 'classes', 'CompileMetadataClass.cls')];
+    helper.cleanUpTestData(testClient, filesToDelete);
+    helper.cleanUpTestProject('compile-metadata');
+  });
+
+  it('should compile a meta.xml file via the metadata api', function(done) {
+
+    helper.unlinkEditor();
+    this.timeout(20000);      
+    
+    helper.setProject(testClient, 'compile-metadata', function() {      
+      helper.createNewMetadata(testClient, 'ApexClass', 'CompileMetadataClass')
+        .then(function() {
+          var metaFileLocation = path.join(helper.baseTestDirectory(), 'workspace', 'compile-metadata', 'src', 'classes', 'CompileMetadataClass.cls-meta.xml');
+          var payload = {
+            files : [metaFileLocation]
+          };
+          testClient.executeCommand('compile-metadata', payload, function(err, response) {
+            should.equal(err, null);
+            response.should.have.property('result');
+            response.result.success.should.equal(true);
             done();
           });
         })
