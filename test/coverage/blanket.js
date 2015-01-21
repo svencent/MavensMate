f028c58025f1f74f942573ba824af030ff8ac902
Joe Ferraro <ferraro.joseph@gmail.com>
2015-01-21 16:36:06 -0500

    remove blanket temporarily

--- test/coverage/blanket.js
+++ test/coverage/blanket.js
@@ -1,7 +1,7 @@
-var path = require('path');
-var libDir = path.join(__dirname, '..', '..', 'lib');
+// var path = require('path');
+// var libDir = path.join(__dirname, '..', '..', 'lib');
 
-require('blanket')({
-  // Only files that match the pattern will be instrumented
-  pattern: libDir
-});
+// require('blanket')({
+//   // Only files that match the pattern will be instrumented
+//   pattern: libDir
+// });
