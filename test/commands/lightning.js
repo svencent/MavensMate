e65b23e3ff000f571b447ec78e349a57039bb01e
Joe Ferraro <ferraro.joseph@gmail.com>
2015-01-27 16:44:22 -0500

    add lightning tests, update resource bundle service, reimplement tests, add blanket dependency

--- test/commands/lightning.js
+++ test/commands/lightning.js
@@ -34,10 +34,10 @@ describe('mavensmate lightning', function(){
   after(function(done) {
     this.timeout(20000);
     var lightningBundlesToDelete = [
-      path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestfoo')
-      // path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo'),
-      // path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestevent'),
-      // path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestinterface')
+      path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestfoo'),
+      path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo'),
+      path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestevent'),
+      path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestinterface')
     ];
 
     helper.cleanUpTestData(testClient, lightningBundlesToDelete)
@@ -86,106 +86,66 @@ describe('mavensmate lightning', function(){
     });    
   });
 
-//   // it('should update a lightning bundle item', function(done) {
-//   //   this.timeout(10000);      
+  it('should create a new lightning component', function(done) {
+    this.timeout(10000);      
     
-//   //   var stylePath = path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestfoo', 'mmunittestfoo.css');
-//   //   fs.outputFileSync(stylePath, '.THIS .foo { color: red; }');
+    var payload = {
+      apiName : 'mmcompunittestfoo',
+      description : 'something_fun',
+      createController: true,
+      createHelper: true,
+      createStyle: true,
+      createDocumentation: true,
+      createRenderer: true
+    };
+    testClient.executeCommand('new-lightning-component', payload, function(err, response) {
+      should.equal(err, null);
+      response.should.have.property('result');
+      response.result.should.equal('Lightning component created successfully');
+      assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo'),  'Lightning bundle not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfoo.cmp'),  'Lightning component not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfoo.auradoc'),  'Lightning doc not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfoo.css'),  'Lightning css not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfooController.js'),  'Lightning controller not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfooHelper.js'),  'Lightning helper not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfooRenderer.js'),  'Lightning renderer not created');
+      done();
+    }); 
+  });
 
-//   //   var payload = {
-//   //     paths : [ stylePath ]
-//   //   };
-//   //   testClient.executeCommand('compile-metadata', payload, function(err, response) {
-//   //     console.log(err);
-//   //     console.log(response);
-//   //     should.equal(err, null);
-//   //     response.should.have.property('result');
-//   //     response.result[0].should.have.property('id');
-//   //     response.result[0].success.should.equal(true);
-//   //     done();
-//   //   });    
-//   // });
-
-//   // it('should refresh a lightning bundle item', function(done) {
-//   //   this.timeout(10000);      
+  it('should create a new lightning interface', function(done) {
+    this.timeout(10000);      
     
-//   //   var stylePath = path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestfoo', 'mmunittestfoo.css');
-//   //   fs.outputFileSync(stylePath, 'THIS IS A LOCAL CHANGE');
+    var payload = {
+      apiName : 'mmunittestinterface',
+      description : 'something_fun'
+    };
+    testClient.executeCommand('new-lightning-interface', payload, function(err, response) {
+      should.equal(err, null);
+      response.should.have.property('result');
+      response.result.should.equal('Lightning interface created successfully');
+      assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestinterface'),  'Lightning bundle not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestinterface', 'mmunittestinterface.intf'),  'Lightning interface not created');
+      done();
+    }); 
+  });
 
-//   //   var payload = {
-//   //     paths : [ stylePath ]
-//   //   };
-//   //   testClient.executeCommand('refresh-metadata', payload, function(err, response) {
-//   //     console.log(err);
-//   //     console.log(response);
-//   //     // should.equal(err, null);
-//   //     // response.should.have.property('result');
-//   //     // response.result[0].should.have.property('id');
-//   //     // response.result[0].success.should.equal(true);
-//   //     done();
-//   //   });    
-//   // });
-
-//   // it('should create a new lightning component', function(done) {
-//   //   this.timeout(10000);      
+  it('should create a lightning event', function(done) {
+    this.timeout(10000);      
     
-//   //   var payload = {
-//   //     apiName : 'mmcompunittestfoo',
-//   //     description : 'something_fun',
-//   //     createController: true,
-//   //     createHelper: true,
-//   //     createStyle: true,
-//   //     createDocumentation: true,
-//   //     createRenderer: true
-//   //   };
-//   //   testClient.executeCommand('new-lightning-component', payload, function(err, response) {
-//   //     should.equal(err, null);
-//   //     response.should.have.property('result');
-//   //     response.result.should.equal('Lightning component created successfully');
-//   //     assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo'),  'Lightning bundle not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfoo.cmp'),  'Lightning component not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfoo.auradoc'),  'Lightning doc not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfoo.css'),  'Lightning css not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfooController.js'),  'Lightning controller not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfooHelper.js'),  'Lightning helper not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmcompunittestfoo', 'mmcompunittestfooRenderer.js'),  'Lightning renderer not created');
-//   //     done();
-//   //   }); 
-//   // });
-
-//   // it('should create a new lightning interface', function(done) {
-//   //   this.timeout(10000);      
-    
-//   //   var payload = {
-//   //     apiName : 'mmunittestinterface',
-//   //     description : 'something_fun'
-//   //   };
-//   //   testClient.executeCommand('new-lightning-interface', payload, function(err, response) {
-//   //     should.equal(err, null);
-//   //     response.should.have.property('result');
-//   //     response.result.should.equal('Lightning interface created successfully');
-//   //     assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestinterface'),  'Lightning bundle not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestinterface', 'mmunittestinterface.intf'),  'Lightning interface not created');
-//   //     done();
-//   //   }); 
-//   // });
-
-//   // it('should create a lightning event', function(done) {
-//   //   this.timeout(10000);      
-    
-//   //   var payload = {
-//   //     apiName : 'mmunittestevent',
-//   //     description : 'something_fun'
-//   //   };
-//   //   testClient.executeCommand('new-lightning-event', payload, function(err, response) {
-//   //     should.equal(err, null);
-//   //     response.should.have.property('result');
-//   //     response.result.should.equal('Lightning event created successfully');
-//   //     assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestevent'),  'Lightning bundle not created');
-//   //     assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestevent', 'mmunittestevent.evt'),  'Lightning event not created');
-//   //     done();
-//   //   }); 
-//   // });
+    var payload = {
+      apiName : 'mmunittestevent',
+      description : 'something_fun'
+    };
+    testClient.executeCommand('new-lightning-event', payload, function(err, response) {
+      should.equal(err, null);
+      response.should.have.property('result');
+      response.result.should.equal('Lightning event created successfully');
+      assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestevent'),  'Lightning bundle not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'lightning', 'src', 'aura', 'mmunittestevent', 'mmunittestevent.evt'),  'Lightning event not created');
+      done();
+    }); 
+  });
 
 });
 
--- test/commands/resource-bundle.js
+++ test/commands/resource-bundle.js
@@ -1,83 +1,96 @@
-// 'use strict';
+'use strict';
 
-// var helper      = require('../test-helper');
-// var chai        = require('chai');
-// var should      = chai.should();
-// var path        = require('path');
-// var fs          = require('fs-extra');
-// var assert      = chai.assert;
+var helper      = require('../test-helper');
+var chai        = require('chai');
+var should      = chai.should();
+var path        = require('path');
+var fs          = require('fs-extra');
+var assert      = chai.assert;
 
-// chai.use(require('chai-fs'));
+chai.use(require('chai-fs'));
 
-// describe('mavensmate resource-bundle', function(){
+describe('mavensmate resource-bundle', function(){
 
-//   var project;
-//   var testClient;
+  var project;
+  var testClient;
 
-//   before(function(done) {
-//     this.timeout(4000);
-//     testClient = helper.createClient('atom');
-//     helper.unlinkEditor();
-//     helper.putTestProjectInTestWorkspace(testClient, 'resource-bundle');
-//     helper.setProject(testClient, 'resource-bundle', function(err, proj) {
-//       project = proj;
-//       done();
-//     });
-//   });
+  before(function(done) {
+    this.timeout(4000);
+    testClient = helper.createClient('atom');
+    helper.unlinkEditor();
+    helper.putTestProjectInTestWorkspace(testClient, 'resource-bundle');
+    helper.setProject(testClient, 'resource-bundle', function(err, proj) {
+      project = proj;
+      done();
+    });
+  });
 
-//   after(function(done) {
-//     helper.cleanUpTestProject('resource-bundle')
-//       .then(function() {
-//         done();
-//       });
-//   });
+  after(function(done) {
+    this.timeout(15000);
+    var filesToDelete = [
+      path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'src', 'staticresources', 'test_resource_bundle.resource')
+    ];
+    helper.cleanUpTestData(testClient, filesToDelete)
+      .then(function() {
+        return helper.cleanUpTestProject('resource-bundle');
+      })
+      .then(function() {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
 
-//   it('should create a resource bundle', function(done) {
-    
-//     this.timeout(50000);
+  it('should create a resource bundle from a local static resource file', function(done) {
+    this.timeout(10000);
   
-//     fs.copySync(
-//       path.join(helper.baseTestDirectory(), 'fixtures', 'test-resource-bundle.zip'), 
-//       path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'src', 'staticresources', 'test-resource-bundle.resource')
-//     );
+    fs.copySync(
+      path.join(helper.baseTestDirectory(), 'fixtures', 'test_resource_bundle.zip'), 
+      path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'src', 'staticresources', 'test_resource_bundle.resource')
+    );
 
-//     fs.copySync(
-//       path.join(helper.baseTestDirectory(), 'fixtures', 'test-resource-bundle.resource-meta.xml'), 
-//       path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'src', 'staticresources', 'test-resource-bundle.resource-meta.xml')
-//     );
+    fs.copySync(
+      path.join(helper.baseTestDirectory(), 'fixtures', 'test_resource_bundle.resource-meta.xml'), 
+      path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'src', 'staticresources', 'test_resource_bundle.resource-meta.xml')
+    );
 
-//     var payload = {
-//       paths : [path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'src', 'staticresources', 'test-resource-bundle.resource')]
-//     };
+    var payload = {
+      paths : [path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'src', 'staticresources', 'test_resource_bundle.resource')]
+    };
 
-//     testClient.executeCommand('new-resource-bundle', payload, function(err, response) {
-//       console.log(err);
-//       console.log(response);
-//       should.equal(err, null);
-//       response.should.have.property('result');
-//       response.result.should.equal('Resource bundle(s) successfully created');
-//       assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'resource-bundles', 'test-resource-bundle.resource'),  'Resource bundle directory not created');
-//       done();
-//     });
-//   });
+    testClient.executeCommand('new-resource-bundle', payload, function(err, response) {
+      console.log(err);
+      console.log(response);
+      should.equal(err, null);
+      response.should.have.property('result');
+      response.result.should.equal('Resource bundle(s) successfully created');
+      assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'resource-bundles', 'test_resource_bundle.resource'),  'Resource bundle directory not created');
+      assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'resource-bundles', 'test_resource_bundle.resource', 'css'),  'Resource bundle css directory not created');
+      assert.isDirectory(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'resource-bundles', 'test_resource_bundle.resource', 'js'),  'Resource bundle js directory not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'resource-bundles', 'test_resource_bundle.resource', 'css', 'bar.css'),  'Resource bundle css file not created');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'resource-bundles', 'test_resource_bundle.resource', 'js', 'foo.js'),  'Resource bundle js file not created');
+      done();
+    });
+  });
 
-//   it('should deploy a resource bundle', function(done) {    
-//     this.timeout(50000);
+  it('should deploy a resource bundle to the server', function(done) {    
+    this.timeout(30000);
 
-//     var payload = {
-//       path : path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'resource-bundles', 'test-resource-bundle.resource')
-//     };
+    var payload = {
+      paths :[ path.join(helper.baseTestDirectory(), 'workspace', 'resource-bundle', 'resource-bundles', 'test_resource_bundle.resource') ]
+    };
 
-//     testClient.executeCommand('deploy-resource-bundle', payload, function(err, response) {
-//       console.log(err);
-//       console.log(response);
-//       should.equal(err, null);
-//       response.should.have.property('result');
-//       response.result.should.equal('Resource bundle successfully deployed');
-//       assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'src', 'staticresources', 'test-resource-bundle.resource'),  'Resource bundle statisresource does not exist');
-//       done();
-//     });    
-//   });
+    testClient.executeCommand('deploy-resource-bundle', payload, function(err, response) {
+      console.log(err);
+      console.log(response);
+      should.equal(err, null);
+      response.should.have.property('result');
+      response.result.should.equal('Resource bundle successfully deployed');
+      assert.isFile(path.join(helper.baseTestDirectory(),'workspace', 'resource-bundle', 'src', 'staticresources', 'test_resource_bundle.resource'),  'Resource bundle staticresource does not exist');
+      done();
+    });    
+  });
 
-// });
+});
 
