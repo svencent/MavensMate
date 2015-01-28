12d30e7db3dd2b91332aa508ccad612096ea575f
Joe Ferraro <ferraro.joseph@gmail.com>
2015-01-28 11:50:10 -0500

    adds session test, adds open metadata and test

---
+++ test/commands/open-metadata.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var helper      = require('../test-helper');
+var chai        = require('chai');
+var should      = chai.should();
+var path        = require('path');
+var fs          = require('fs-extra');
+
+describe('mavensmate open-metadata', function(){
+
+  var project;
+  var testClient;
+
+  before(function(done) {
+    this.timeout(4000);
+    testClient = helper.createClient('atom');
+    helper.unlinkEditor();
+    helper.putTestProjectInTestWorkspace(testClient, 'open-metadata');
+    helper.setProject(testClient, 'open-metadata', function(err, proj) {
+      project = proj;
+      done();
+    });
+  });
+
+  after(function(done) {
+    this.timeout(15000);
+    var filesToDelete = [
+      path.join(helper.baseTestDirectory(),'workspace', 'open-metadata', 'src', 'pages', 'OpenMetadataPage.page')
+    ];
+    helper.cleanUpTestData(testClient, filesToDelete)
+      .then(function() {
+        return helper.cleanUpTestProject('open-metadata');
+      })
+      .then(function() {
+        done();
+      });
+  });
+
+  it('should return the Visualforce page preview url', function(done) {
+    this.timeout(20000);      
+
+    helper.createNewMetadata(testClient, 'ApexPage', 'OpenMetadataPage', 'ApexPage.page', { api_name : 'OpenMetadataPage' } )
+      .then(function() {          
+        var payload = {
+          paths : [ path.join(helper.baseTestDirectory(),'workspace', 'open-metadata', 'src', 'pages', 'OpenMetadataPage.page') ],
+          preview: true
+        };
+        testClient.executeCommand('open-metadata', payload, function(err, response) {
+          console.log(err);
+          console.log(response);
+          should.equal(err, null);
+          response.should.have.property('result');
+          response.result.should.have.property('OpenMetadataPage.page');
+          response.result['OpenMetadataPage.page'].indexOf('secur/frontdoor.jsp?sid=').should.be.at.least(0);
+          done();
+        });
+      });
+  });
+});
---
+++ test/commands/session.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var helper      = require('../test-helper');
+var chai        = require('chai');
+var should      = chai.should();
+var path        = require('path');
+var fs          = require('fs-extra');
+
+chai.use(require('chai-fs'));
+
+describe('mavensmate session', function() {
+
+  var testClient;
+
+  before(function(done) {
+    this.timeout(4000);
+    testClient = helper.createClient('atom');
+    helper.unlinkEditor();
+    done();
+  });
+
+  it('should initiate new salesforce session', function(done) {
+    this.timeout(20000);      
+
+    var payload = {
+      username: process.env.username || 'mm@force.com',
+      password: process.env.password || 'force',
+      orgType: 'developer',
+      subscription: ['ApexClass']
+    };
+
+    testClient.executeCommand('session', payload, function(err, response) {
+      should.equal(err, null);
+      response.should.have.property('result');
+      response.result.should.have.property('sid');
+      response.result.should.have.property('urls');
+      response.result.should.have.property('metadataTypes');
+      response.result.should.have.property('index');
+      done();
+    });
+  });
+
+  it('should fail to initiate new salesforce session', function(done) {
+    this.timeout(20000);      
+
+    var payload = {
+      username: 'thiswontwork@foo.com',
+      password: 'barbar',
+      orgType: 'developer'
+    };
+
+    testClient.executeCommand('session', payload, function(err, response) {
+      should.not.equal(err, null);
+      err.should.have.property('result');
+      err.should.have.property('error');
+      err.result.should.equal('Could not create new Salesforce session');
+      err.error.should.equal('Could not log in to Salesforce.com: Error: INVALID_LOGIN: Invalid username, password, security token; or user locked out.');
+      done();
+    });
+  });
+});
+
