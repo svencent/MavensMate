e77ac7a2afc26d6b9ad2254ea7a621bfa1675413
Joe Ferraro <ferraro.joseph@gmail.com>
2015-02-03 23:44:36 -0500

    fixes coverage command to include org-wide or class-specific, beefs up tests

--- test/commands/run-tests.js
+++ test/commands/run-tests.js
@@ -4,6 +4,7 @@ var helper      = require('../test-helper');
 var chai        = require('chai');
 var should      = chai.should();
 var path        = require('path');
+var fs          = require('fs-extra');
 
 describe('mavensmate run-tests', function(){
 
@@ -23,7 +24,10 @@ describe('mavensmate run-tests', function(){
 
   after(function(done) {
     this.timeout(10000);
-    var filesToDelete = [path.join(helper.baseTestDirectory(),'workspace', 'run-tests', 'src', 'classes', 'RunTestsApexClass.cls')];
+    var filesToDelete = [
+      path.join(helper.baseTestDirectory(),'workspace', 'run-tests', 'src', 'classes', 'RunTestsApexClass.cls'),
+      path.join(helper.baseTestDirectory(),'workspace', 'run-tests', 'src', 'classes', 'CoverMe.cls')
+    ];
     helper.cleanUpTestData(testClient, filesToDelete)
       .then(function() {
         return helper.cleanUpTestProject('run-tests');
@@ -44,22 +48,76 @@ describe('mavensmate run-tests', function(){
     helper.getNewMetadataPayload('ApexClass', 'RunTestsApexClass', 'UnitTestApexClass.cls')
       .then(function(payload) {
         testClient.executeCommand('new-metadata', payload, function() {
-          var testPayload = {
-            classes: [ 'RunTestsApexClass.cls' ]
-          };
-          testClient.executeCommand('run-tests', testPayload, function(err, response) {
-            should.equal(err, null);
-            response.should.have.property('result');
-            response.result.should.have.property('testResults');
-            response.result.should.have.property('coverageResults');
-            response.result.testResults.should.have.property('RunTestsApexClass');
-            response.result.coverageResults.should.have.property('classes');
-            response.result.coverageResults.should.have.property('triggers');
-            done();
-          }); 
+          
+          helper.getNewMetadataPayload('ApexClass', 'CoverMe', 'ApexClass.cls')
+            .then(function(payload) {
+              
+              testClient.executeCommand('new-metadata', payload, function() {
+                
+                var coverMePath = path.join(helper.baseTestDirectory(),'workspace', 'run-tests', 'src', 'classes', 'CoverMe.cls');
+                fs.outputFileSync(coverMePath, 'public class CoverMe { public static Boolean doSomething() { return true; } }');
+
+                var testClassPath = path.join(helper.baseTestDirectory(),'workspace', 'run-tests', 'src', 'classes', 'RunTestsApexClass.cls');
+                fs.outputFileSync(testClassPath, '@IsTest public class RunTestsApexClass { @IsTest public static void myTest() { System.assert(CoverMe.doSomething() == true); } }');
+
+                var compilePayload = {
+                  paths: [ coverMePath, testClassPath ]
+                };
+                
+                testClient.executeCommand('compile-metadata', compilePayload, function() {
+                  var testPayload = {
+                    classes: [ 'RunTestsApexClass.cls' ]
+                  };
+
+                  testClient.executeCommand('run-tests', testPayload, function(err, response) {
+                    should.equal(err, null);
+                    response.should.have.property('result');
+                    response.result.should.have.property('testResults');
+                    response.result.should.have.property('coverageResults');
+                    response.result.testResults.should.have.property('RunTestsApexClass');
+                    response.result.testResults.RunTestsApexClass.Status.should.equal('Completed');
+                    response.result.testResults.RunTestsApexClass.results[0].Outcome.should.equal('Pass');
+                    response.result.testResults.RunTestsApexClass.results[0].MethodName.should.equal('myTest');
+                    response.result.coverageResults.should.have.property('classes');
+                    response.result.coverageResults.should.have.property('triggers');
+                    response.result.coverageResults.classes.CoverMe.coveredLines.length.should.equal(1);
+                    response.result.coverageResults.classes.CoverMe.coveredLines[0].should.equal(1);
+                    response.result.coverageResults.classes.CoverMe.percentCovered.should.equal(100);
+                    done(); 
+                  });                
+                });
+
+              });
+            });
         });
       })
       .done();
   });
 
+  describe('coverage', function(){
+    it('should get coverage for a specific class', function(done) {
+      this.timeout(20000);
+      var coverageClasses = [path.join(helper.baseTestDirectory(),'workspace', 'run-tests', 'src', 'classes', 'CoverMe.cls')];  
+      testClient.executeCommand('get-coverage', { paths: coverageClasses  }, function(err, response) {
+        should.equal(err, null);
+        response.should.have.property('result');
+        response.result.should.have.property('CoverMe.cls');
+        response.result['CoverMe.cls'].coveredLines.length.should.equal(1);
+        response.result['CoverMe.cls'].coveredLines[0].should.equal(1);
+        response.result['CoverMe.cls'].totalLines.should.equal(1);
+        response.result['CoverMe.cls'].percentCovered.should.equal(100);
+        done();
+      });
+    });
+
+    it('should get org-wide test coverage', function(done) {
+      this.timeout(20000);
+      testClient.executeCommand('get-coverage', { global: true  }, function(err, response) {
+        should.equal(err, null);
+        response.should.have.property('result');
+        response.result.should.be.a('number');
+        done();
+      });
+    });
+  });
 });
