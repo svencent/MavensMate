204a66ceeab85136bf6617570405b239c3fa00ac
Joe Ferraro <ferraro.joseph@gmail.com>
2015-07-06 17:26:47 -0400

    tweak parallelism script

--- ci.sh
+++ ci.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+NODE_TOTAL=${CIRCLE_NODE_TOTAL:-1}
+NODE_INDEX=${CIRCLE_NODE_INDEX:-0}
+
 i=0
-files=( )
-for file in $(find ./test/integration/commands -name "*.js")
+files=()
+for file in $(find ./test/integration/commands -name "*.js" | sort)
 do
-  if [[ $(($i % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX ]]
+  if [ $(($i % ${NODE_TOTAL})) -eq ${NODE_INDEX} ]
   then
     files+=" $file"
   fi
