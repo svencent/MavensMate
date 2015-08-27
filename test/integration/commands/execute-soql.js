821273cd4b4bdfbeb35f226f6cc22a190eacceb8
Joe Ferraro <ferraro.joseph@gmail.com>
2015-08-27 08:21:32 -0400

    adds execute-soql command

---
+++ test/integration/commands/execute-soql.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var helper      = require('../../test-helper');
+var chai        = require('chai');
+var should      = chai.should();
+var path        = require('path');
+
+describe('mavensmate execute-soql', function(){
+
+  var project;
+  var testClient;
+
+  before(function(done) {
+    this.timeout(8000);
+    testClient = helper.createClient('unittest');
+    helper.unlinkEditor();
+    helper.putTestProjectInTestWorkspace(testClient, 'execute-soql');
+    helper.addProject(testClient, 'execute-soql')
+      .then(function(proj) {
+        project = proj;
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
+  after(function(done) {
+    helper.cleanUpTestProject('execute-soql');
+    done();
+  });
+
+  it('should successfully execute soql query', function(done) {
+    this.timeout(3000);      
+
+    testClient.executeCommand('execute-soql', { soql: 'SELECT ID From Account LIMIT 1' })
+      .then(function(res) {
+        res.should.have.property('records');
+        res.should.have.property('done');
+        res.should.have.property('totalSize');
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
+  it('should fail to execute soql query', function(done) {
+    this.timeout(3000);      
+
+    testClient.executeCommand('execute-soql', { soql: 'SELECT From Account LIMIT 1' })
+      .catch(function(err) {
+        err.errorCode.should.contain('MALFORMED_QUERY');
+        done();
+      });
+  });
+});
