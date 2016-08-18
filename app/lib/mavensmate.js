c53ec3ac216ec1f27439f07d409759d6f4467247
Joe Ferraro <ferraro.joseph@gmail.com>
2016-08-18 14:19:44 -0400

    expose new module for mavensmate-app to require

---
+++ app/lib/mavensmate.js
@@ -0,0 +1,31 @@
+/*
+ * @file global MavensMate process
+ * @author Joe Ferraro <@joeferraro>
+ */
+
+var mavensmate = exports;
+
+var Promise = require('bluebird');
+mavensmate.startServer = function(opts) {
+  return new Promise(function(resolve, reject) {
+    opts = opts || {};
+    try {
+      var server = require('../index');
+      var port = opts.port || '56248';
+      var verbose = opts.verbose || false;
+
+      var res = server.start({
+        port: port,
+        verbose: opts.verbose,
+        openWindowFn: opts.openWindowFn
+      });
+      console.log('MavensMate server running on port: '+port);
+      res.config = require('../config');
+      res.logger = require('winston');
+      resolve(res);
+    } catch(e) {
+      console.error('MavensMate server failed to start: '+err.message);
+      reject(e);
+    }
+  });
+};
