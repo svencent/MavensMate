d400b6bad2a4827bb4045278fcc77fb001dee6f8
Joe Ferraro <ferraro.joseph@gmail.com>
2016-08-21 22:50:41 -0400

    fix url for creating new metadata

--- app/lib/commands/metadata/new-metadata.js
+++ app/lib/commands/metadata/new-metadata.js
@@ -28,7 +28,7 @@ Command.prototype.execute = function() {
   var self = this;
   return new Promise(function(resolve, reject) {
     if (self.isUICommand()) {
-      self.editorService.launchUI('metadata/new/'+self.payload.args.type, { pid: self.getProject().settings.id, type: self.payload.args.type })
+      self.editorService.launchUI('metadata/'+self.payload.args.type+'/new', { pid: self.getProject().settings.id, type: self.payload.args.type })
         .then(function() {
           resolve('Success');
         })
