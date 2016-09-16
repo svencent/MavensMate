e9645d4bf85caa291ab26a82937bf5b7edcbc9f0
Joe Ferraro <ferraro.joseph@gmail.com>
2016-09-16 09:50:48 -0400

    initial path thoughts ... LOTS to do.

---
+++ test/unit/path.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var fs                = require('fs-extra');
+var path              = require('path');
+var helper            = require('../test-helper');
+var ProjectPath       = require('../../app/lib/path');
+var chai              = require('chai');
+var should            = chai.should();
+var logger            = require('winston');
+var sinon             = require('sinon');
+
+describe('mavensmate project-path', function(){
+
+  var project;
+  var sandbox;
+  var commandExecutorStub;
+
+  beforeEach(function(done) {
+    sandbox = sinon.sandbox.create();
+    // commandExecutorStub = sandbox.stub(app.get('commandExecutor'), 'execute');
+    // commandExecutorStub.resolves({ success: true });
+    helper.stubSalesforceClient(sandbox);
+    helper.bootstrapEnvironment();
+    helper.putTestProjectInTestWorkspace('path-test');
+    helper.addProject('path-test')
+      .then(function(proj) {
+        project = proj;
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+  });
+
+  afterEach(function(done) {
+    sandbox.restore();
+    helper.cleanUpProject('path-test');
+    done();
+  });
+
+  it('should be an ApexClass', function(done) {
+    var apexClassPath = path.join(helper.baseTestDirectory(), 'workspace', 'path-test', 'src', 'classes', 'foo.cls');
+    fs.outputFileSync(apexClassPath, '');
+    var myPath = new ProjectPath({
+      project:project,
+      path:apexClassPath
+    });
+    logger.debug(myPath);
+    done();
+  });
+
+
+});
